171368c730a17ba3fff426eef59c9ff16c488c39
sanaullah129 <109277244+sanaullah129@users.noreply.github.com>
2023-08-08 22:27:50 +0530

    Update and delete query issues

--- server/routes/task.ts
+++ server/routes/task.ts
@@ -3,119 +3,7 @@ import pool from '../db';
 
 const router = express.Router();
 
-// // Create a task
-// router.post("/create-task", async (req: Request, res: Response) => {
-//   try {
-//     const { description } = req.body;
-//     const newTask = await pool.query("SELECT create_task($1) as task_id", [description]);
-//     res.json(newTask.rows[0]);
-//   } catch (error) {
-//     res.json((error as Error).message);
-//   }
-// });
-// // router.post("/create-task", async (req: Request, res: Response) => {
-// //   try {
-// //     const { description } = req.body;
-// //     const newTask = await pool.query("INSERT INTO tasks (description) VALUES($1) RETURNING *", [description]);
-// //     res.json(newTask.rows[0]);
-// //   } catch (error) {
-// //     res.json((error as Error).message); // Type assertion to Error
-// //   }
-// // });
-
-// // Get all tasks
-// router.get("/get-tasks", async (req: Request, res: Response) => {
-//   try {
-//     const allTasks = await pool.query("SELECT * FROM get_tasks()");
-//     res.json(allTasks.rows);
-//   } catch (error) {
-//     res.json((error as Error).message);
-//   }
-// });
-// // router.get("/get-tasks", async (req: Request, res: Response) => {
-// //   try {
-// //     const allTasks = await pool.query("SELECT * FROM tasks");
-// //     res.json(allTasks.rows);
-// //   } catch (error) {
-// //     res.json((error as Error).message); // Type assertion to Error
-// //   }
-// // });
-
-
-// // Get one task
-// router.get("/get-task/:id", async (req: Request, res: Response) => {
-//   try {
-//     const { id } = req.params;
-    
-//     const query = `SELECT description FROM get_one_task($1) AS t(description VARCHAR(255))`;
-
-//     const result = await pool.query(query, [id]);
-//     res.json(result.rows[0]);
-
-//   } catch (error) {
-//     res.json((error as Error).message);
-//   }
-// });
-
-
-// // router.get("/get-task/:id", async (req: Request, res: Response) => {
-// //   try {
-// //     const { id } = req.params;
-// //     const task = await pool.query("SELECT * FROM tasks WHERE task_id = $1", [id]);
-// //     res.json(task.rows[0]);
-// //   } catch (error) {
-// //     res.json((error as Error).message); // Type assertion to Error
-// //   }
-// // });
-
-
-// // Update a task
-// router.put("/update-task/:id", async (req: Request, res: Response) => {
-//   try {
-//     const { id } = req.params;
-//     const { description } = req.body;
-//     const updateTask = await pool.query("SELECT update_task($1, $2) AS message", [id, description]);
-//     res.json(updateTask.rows[0]);
-//   } catch (error) {
-//     res.json((error as Error).message);
-//   }
-// });
-
-// // router.put("/update-task/:id", async (req: Request, res: Response) => {
-// //   try {
-// //     const { id } = req.params;
-// //     const { description } = req.body;
-// //     const updateTask = await pool.query("UPDATE tasks SET description = $1 WHERE task_id = $2", [description, id]);
-// //     res.json("Task was updated");
-// //   } catch (error) {
-// //     res.json((error as Error).message); // Type assertion to Error
-// //   }
-// // });
-
-
-// // Delete a task
-// router.delete("/delete-task/:id", async (req: Request, res: Response) => {
-//   try {
-//     const { id } = req.params;
-//     const deleteTask = await pool.query("SELECT delete_task($1) as message", [id]);
-//     res.json(deleteTask.rows[0]);
-//   } catch (error) {
-//     res.json((error as Error).message);
-//   }
-// });
-// // router.delete("/delete-task/:id", async (req: Request, res: Response) => {
-// //   try {
-// //     const { id } = req.params;
-// //     const deleteTask = await pool.query("DELETE FROM tasks WHERE task_id = $1", [id]);
-// //     res.json("Task was deleted");
-// //   } catch (error) {
-// //     res.json((error as Error).message); // Type assertion to Error
-// //   }
-// // });
-
-// export default router;
-
-// Create a new task
+// Create a task
 router.post("/create-task", async (req: Request, res: Response) => {
   try {
     const { description } = req.body;
@@ -131,6 +19,15 @@ router.post("/create-task", async (req: Request, res: Response) => {
     res.status(500).json({ error: (error as Error).message });
   }
 });
+// router.post("/create-task", async (req: Request, res: Response) => {
+//   try {
+//     const { description } = req.body;
+//     const newTask = await pool.query("INSERT INTO tasks (description) VALUES($1) RETURNING *", [description]);
+//     res.json(newTask.rows[0]);
+//   } catch (error) {
+//     res.json((error as Error).message); // Type assertion to Error
+//   }
+// })
 
 // Get all tasks
 router.get("/get-tasks", async (req: Request, res: Response) => {
@@ -146,6 +43,14 @@ router.get("/get-tasks", async (req: Request, res: Response) => {
     res.status(500).json({ error: (error as Error).message });
   }
 });
+// router.get("/get-tasks", async (req: Request, res: Response) => {
+//   try {
+//     const allTasks = await pool.query("SELECT * FROM tasks");
+//     res.json(allTasks.rows);
+//   } catch (error) {
+//     res.json((error as Error).message); // Type assertion to Error
+//   }
+// })
 
 // Get one task
 router.get("/get-task/:id", async (req: Request, res: Response) => {
@@ -163,6 +68,15 @@ router.get("/get-task/:id", async (req: Request, res: Response) => {
     res.status(500).json({ error: (error as Error).message });
   }
 });
+// router.get("/get-task/:id", async (req: Request, res: Response) => {
+//   try {
+//     const { id } = req.params;
+//     const task = await pool.query("SELECT * FROM tasks WHERE task_id = $1", [id]);
+//     res.json(task.rows[0]);
+//   } catch (error) {
+//     res.json((error as Error).message); // Type assertion to Error
+//   }
+// })
 
 // Update a task
 router.put("/update-task/:id", async (req: Request, res: Response) => {
@@ -181,6 +95,16 @@ router.put("/update-task/:id", async (req: Request, res: Response) => {
     res.status(500).json({ error: (error as Error).message });
   }
 });
+// router.put("/update-task/:id", async (req: Request, res: Response) => {
+//   try {
+//     const { id } = req.params;
+//     const { description } = req.body;
+//     const updateTask = await pool.query("UPDATE tasks SET description = $1 WHERE task_id = $2", [description, id]);
+//     res.json("Task was updated");
+//   } catch (error) {
+//     res.json((error as Error).message); // Type assertion to Error
+//   }
+// })
 
 // Delete a task
 router.delete("/delete-task/:id", async (req: Request, res: Response) => {
@@ -198,6 +122,17 @@ router.delete("/delete-task/:id", async (req: Request, res: Response) => {
     res.status(500).json({ error: (error as Error).message });
   }
 });
-
+// router.delete("/delete-task/:id", async (req: Request, res: Response) => {
+//   try {
+//     const { id } = req.params;
+//     const deleteTask = await pool.query("DELETE FROM tasks WHERE task_id = $1", [id]);
+//     res.json("Task was deleted");
+//   } catch (error) {
+//     res.json((error as Error).message); // Type assertion to Error
+//   }
+// })
 
 export default router;
+
+
+
